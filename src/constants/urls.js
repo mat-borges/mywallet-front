c6314b9347e4061d1482aac031a5fb8416c2eaa6
Mateus Borges <borgesm.work@gmail.com>
2022-11-21 17:16:56 -0300

    fix!: fix api url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'https://mywallet-api-drxc.onrender.com';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://mywallet-api-drxc.onrender.com';
